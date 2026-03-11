309930f636661449a1866ff32c025a3307ac026d
John Wehr <johnwehr@gmail.com>
2026-03-11 16:59:33 -0400

    Change branch name

--- plugins/runtime/bin/transcript-watcher.mjs
+++ plugins/runtime/bin/transcript-watcher.mjs
@@ -30,9 +30,9 @@ var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__ge
   mod
 ));
 
-// ../../../../../../../node_modules/ws/lib/constants.js
+// ../../../node_modules/ws/lib/constants.js
 var require_constants = __commonJS({
-  "../../../../../../../node_modules/ws/lib/constants.js"(exports, module) {
+  "../../../node_modules/ws/lib/constants.js"(exports, module) {
     "use strict";
     var BINARY_TYPES = ["nodebuffer", "arraybuffer", "fragments"];
     var hasBlob = typeof Blob !== "undefined";
@@ -53,9 +53,9 @@ var require_constants = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/buffer-util.js
+// ../../../node_modules/ws/lib/buffer-util.js
 var require_buffer_util = __commonJS({
-  "../../../../../../../node_modules/ws/lib/buffer-util.js"(exports, module) {
+  "../../../node_modules/ws/lib/buffer-util.js"(exports, module) {
     "use strict";
     var { EMPTY_BUFFER } = require_constants();
     var FastBuffer = Buffer[Symbol.species];
@@ -128,9 +128,9 @@ var require_buffer_util = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/limiter.js
+// ../../../node_modules/ws/lib/limiter.js
 var require_limiter = __commonJS({
-  "../../../../../../../node_modules/ws/lib/limiter.js"(exports, module) {
+  "../../../node_modules/ws/lib/limiter.js"(exports, module) {
     "use strict";
     var kDone = Symbol("kDone");
     var kRun = Symbol("kRun");
@@ -178,9 +178,9 @@ var require_limiter = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/permessage-deflate.js
+// ../../../node_modules/ws/lib/permessage-deflate.js
 var require_permessage_deflate = __commonJS({
-  "../../../../../../../node_modules/ws/lib/permessage-deflate.js"(exports, module) {
+  "../../../node_modules/ws/lib/permessage-deflate.js"(exports, module) {
     "use strict";
     var zlib = __require("zlib");
     var bufferUtil = require_buffer_util();
@@ -561,9 +561,9 @@ var require_permessage_deflate = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/validation.js
+// ../../../node_modules/ws/lib/validation.js
 var require_validation = __commonJS({
-  "../../../../../../../node_modules/ws/lib/validation.js"(exports, module) {
+  "../../../node_modules/ws/lib/validation.js"(exports, module) {
     "use strict";
     var { isUtf8 } = __require("buffer");
     var { hasBlob } = require_constants();
@@ -762,9 +762,9 @@ var require_validation = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/receiver.js
+// ../../../node_modules/ws/lib/receiver.js
 var require_receiver = __commonJS({
-  "../../../../../../../node_modules/ws/lib/receiver.js"(exports, module) {
+  "../../../node_modules/ws/lib/receiver.js"(exports, module) {
     "use strict";
     var { Writable } = __require("stream");
     var PerMessageDeflate = require_permessage_deflate();
@@ -1354,9 +1354,9 @@ var require_receiver = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/sender.js
+// ../../../node_modules/ws/lib/sender.js
 var require_sender = __commonJS({
-  "../../../../../../../node_modules/ws/lib/sender.js"(exports, module) {
+  "../../../node_modules/ws/lib/sender.js"(exports, module) {
     "use strict";
     var { Duplex } = __require("stream");
     var { randomFillSync } = __require("crypto");
@@ -1842,9 +1842,9 @@ var require_sender = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/event-target.js
+// ../../../node_modules/ws/lib/event-target.js
 var require_event_target = __commonJS({
-  "../../../../../../../node_modules/ws/lib/event-target.js"(exports, module) {
+  "../../../node_modules/ws/lib/event-target.js"(exports, module) {
     "use strict";
     var { kForOnEventAttribute, kListener } = require_constants();
     var kCode = Symbol("kCode");
@@ -2071,9 +2071,9 @@ var require_event_target = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/extension.js
+// ../../../node_modules/ws/lib/extension.js
 var require_extension = __commonJS({
-  "../../../../../../../node_modules/ws/lib/extension.js"(exports, module) {
+  "../../../node_modules/ws/lib/extension.js"(exports, module) {
     "use strict";
     var { tokenChars } = require_validation();
     function push(dest, name, elem) {
@@ -2224,9 +2224,9 @@ var require_extension = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/websocket.js
+// ../../../node_modules/ws/lib/websocket.js
 var require_websocket = __commonJS({
-  "../../../../../../../node_modules/ws/lib/websocket.js"(exports, module) {
+  "../../../node_modules/ws/lib/websocket.js"(exports, module) {
     "use strict";
     var EventEmitter = __require("events");
     var https = __require("https");
@@ -3110,9 +3110,9 @@ var require_websocket = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/stream.js
+// ../../../node_modules/ws/lib/stream.js
 var require_stream = __commonJS({
-  "../../../../../../../node_modules/ws/lib/stream.js"(exports, module) {
+  "../../../node_modules/ws/lib/stream.js"(exports, module) {
     "use strict";
     var WebSocket2 = require_websocket();
     var { Duplex } = __require("stream");
@@ -3208,9 +3208,9 @@ var require_stream = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/subprotocol.js
+// ../../../node_modules/ws/lib/subprotocol.js
 var require_subprotocol = __commonJS({
-  "../../../../../../../node_modules/ws/lib/subprotocol.js"(exports, module) {
+  "../../../node_modules/ws/lib/subprotocol.js"(exports, module) {
     "use strict";
     var { tokenChars } = require_validation();
     function parse(header) {
@@ -3253,9 +3253,9 @@ var require_subprotocol = __commonJS({
   }
 });
 
-// ../../../../../../../node_modules/ws/lib/websocket-server.js
+// ../../../node_modules/ws/lib/websocket-server.js
 var require_websocket_server = __commonJS({
-  "../../../../../../../node_modules/ws/lib/websocket-server.js"(exports, module) {
+  "../../../node_modules/ws/lib/websocket-server.js"(exports, module) {
     "use strict";
     var EventEmitter = __require("events");
     var http = __require("http");
@@ -3651,7 +3651,7 @@ import { access, open, unlink } from "node:fs/promises";
 import * as net from "node:net";
 import { join as join2 } from "node:path";
 
-// ../../../../../../../node_modules/ws/wrapper.mjs
+// ../../../node_modules/ws/wrapper.mjs
 var import_stream = __toESM(require_stream(), 1);
 var import_receiver = __toESM(require_receiver(), 1);
 var import_sender = __toESM(require_sender(), 1);
